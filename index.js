a397b68dc42a5a60f56c4d5a3ae6a201816ca517
AdityaDeshwal <95560509+AdityaDeshwal@users.noreply.github.com>
2022-06-29 02:45:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var n=Math.random();
 var randomNumber=Math.floor(n*6)+1;
-document.querySelector(".img1").setAttribute("src","Dicee Challenge - Starting Files/images/dice"+randomNumber+".png");
+document.querySelector(".img1").setAttribute("src","dice"+randomNumber+".png");
 var n2=Math.random();
 var randomNumber2=Math.floor(n2*6)+1;
-document.querySelector(".img2").setAttribute("src","Dicee Challenge - Starting Files/images/dice"+randomNumber2+".png");
+document.querySelector(".img2").setAttribute("src","dice"+randomNumber2+".png");
 if(randomNumber>randomNumber2){
     document.querySelector(".container h1").textContent="🎊Player 1 wins!";
 }
